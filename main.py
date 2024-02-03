5df0e1c603696ff8672eba5fc3ee6ac0cd0e16eb
Shubbair <hussainsalih.github@gmail.com>
2024-02-03 19:34:11 +0300

    update main file

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
+import json
+import pandas as pd
 import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
 
 genai.configure(api_key=os.environ.get('API_KEY'))
+
+qa_data = open('Sistani_QA.json')
+qa_data = json.load(qa_data)
+
+data_frame = pd.DataFrame(qa_data)
+data_frame.head()
